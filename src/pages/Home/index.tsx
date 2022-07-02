9b57c2d152b3c0ec6fdd1d1d149f29f175061546
Artur Rodrigues <artrodrigues19@gmail.com>
2022-07-02 06:55:36 -0300

    1.3 iniciando pagina home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+interface HomeProps {
+   children?: React.ReactNode;
+}
+
+export function Home({ }: HomeProps): JSX.Element {
+   return (
+      <div>
+
+      </div>
+   );
+}
+
+export default Home;
